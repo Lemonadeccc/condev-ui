6f86f5a99ea2196d6a900c7e80a587fedddede1e
Lemonadeccc <zwjhb12@163.com>
2024-10-28 22:12:28 +0800

    feat:add vitest for components.

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
+import type { ButtonProps } from "./types";
+import { ref } from "vue";
+
 defineOptions({
   name: "CdButton",
 });
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: "button",
+  nativeType: "button",
+});
+
+const slot = defineSlots();
+
+const _ref = ref<HTMLButtonElement>();
 </script>
+
 <template>
-  <button style="background-color: blue; color: red">this is a button</button>
+  <component
+    :is="props.tag"
+    class="cd-button"
+    :class="[
+      `cd-button--${type}`,
+      `cd-button--${size}`,
+      {
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle,
+        'is-disabled': disabled,
+        'is-loading': loading,
+      },
+    ]"
+    ref="_ref"
+    :type="tag === 'button' ? nativeType : void 0"
+    :disabled="disabled || loading ? true : void 0"
+  >
+    <slot />
+  </component>
 </template>
