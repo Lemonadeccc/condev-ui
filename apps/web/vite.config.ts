dc8ce28cb6f6c21c38e8ca40f7022e22a40e5453
Lemonadeccc <zwjhb12@163.com>
2025-04-12 21:40:52 +0800

    fix: :bug: change the config of workflow & change the verison of react in web.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,31 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/condev-ui/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
